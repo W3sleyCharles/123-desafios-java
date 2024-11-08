1aae3eedd32ddd070af5e7a852c34201ecfb7a34
Wesley <wesleycharles01@hotmail.com>
2024-11-08 08:55:07 -0300

    Primeiro Desafio

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # desafio-logica-java
 Exercícios de lógica da programação em Java
 
-- [ ]  1 . Calcule a média de duas notas e mostre a média aritmética obtida.
+- [x]  1 . Calcule a média de duas notas e mostre a média aritmética obtida.
 - [ ]  2 . Leia o nome de uma pessoa, a sua idade e o seu salário, e mostre essas informações.
 - [ ]  3 . Leia dois números, efetue as 4 operações matemáticas básicas e mostre os resultados.
 - [ ]  4 . Duas variáveis (A e B) possuem valores distintos( A = 5 e B = 10). Crie um algoritmo que armazene estes dois valores nessas duas variáveis, e efetue a troca dos valores de forma que a variável A passe a possuir o valor de B e que a variável B passe a possuir o valor de A. Por fim, apresente estes valores trocados.
---
+++ desafios/desafio_1/desafio_1.java
@@ -0,0 +1,39 @@
+// 1 . Calcule a média de duas notas e mostre a média aritmética obtida.
+
+// Importando biblioteca para realizar um input de dados
+import java.util.Scanner;
+
+public class desafio_1 {
+
+    public static void main(String[] args) {
+
+    // Iniciando o Scanner para adicionar valor para as variáveis "primeiraNota e segundaNota"    
+    Scanner scanner = new Scanner(System.in);
+
+    // Definindo o valor da primeira nota via input
+    System.out.println("Digita a primeira nota: ");
+    int primeiraNota = scanner.nextInt();
+    
+    // Definindo o valor da segunda nota via input
+    System.out.println("Digite a segunda nota: ");
+    int segundaNota = scanner.nextInt();
+
+    // Fechando o scanner
+    scanner.close();
+    
+    // Entregando o resultado das notas
+    System.out.println("A média de nota dos alunos é: " + calculo(primeiraNota, segundaNota));
+
+
+
+        
+    }
+    // Método para realizar a soma das notas e a divisão para a média
+    public static int calculo (int primeiraNota, int segundaNota) {
+
+        // Formúla para realizar o calculo que após finalizado retorna o valor esperado
+        return (primeiraNota + segundaNota) / 2;  
+
+    }
+
+}
