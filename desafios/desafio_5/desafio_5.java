5107044b5fb350574fc51b70194d82742d40e04c
Wesley <wesleycharles01@hotmail.com>
2025-07-16 23:03:23 -0300

    voltando e atualização

---
+++ desafios/desafio_5/desafio_5.java
@@ -0,0 +1,17 @@
+/* 5 . O custo ao consumidor de um carro novo, é a soma do custo de fábrica com a percentagem do revendedor e com o custo dos impostos 
+(aplicados ao custo de fábrica) .Supondo que a percentagem do revendedor seja 25% e que os impostos custam 45% do custo de fábrica. 
+Faça um algoritmo que leia o valor de custo de fábrica e determine o preço final do automóvel (preço ao consumidor). */
+
+package desafios.desafio_5;
+
+public class desafio_5 {
+
+    int custoFabrica;
+    double percentualRevendedor = 0.25;
+    double percentualImpostos = 0.45;
+    double custoRevendedor;
+    double custoImpostos;
+    double custoFinal;
+    double custoTotal;
+    
+}
